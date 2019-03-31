98874ababe4a73d7dacf8b47b5e0ae997d4bf0ed
Chai <936542789@qq.com>
2019-03-31 10:28:42 +0800

    first commit

---
+++ GUI.py
@@ -0,0 +1,117 @@
+import  sys
+from PyQt5 import QtGui
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication,QPushButton,QDialog,QGridLayout,QLabel,QWidget,QInputDialog,QLineEdit,QVBoxLayout,QTextEdit
+from PyQt5.QtCore import QCoreApplication
+import requests
+import json
+import cv2
+import btnfunc
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.title="NASAService"
+        self.InitWindow()
+
+
+    # 主窗口界面设置
+    def InitWindow(self):
+        self.setWindowIcon(QtGui.QIcon("icon.jpg"))
+        self.setWindowTitle(self.title)
+        self.resize(400, 800)
+        self.btnAPOD = QPushButton('APOD', self)
+        self.btnNEO = QPushButton('NEO', self)
+        self.btnMARS = QPushButton('MARS', self)
+        self.btnQUIT = QPushButton('EXIT', self)
+        self.label = QLabel(" ")
+        self.text=QTextEdit()
+        self.label2=QLabel()
+        self.label2.setPixmap(QPixmap('show.jpg').scaled(480,680,aspectRatioMode=1))
+
+
+
+
+
+        # 布局设定
+        #全局布局
+        wlayout=QVBoxLayout(self)
+
+        #局部布局
+        layout1 = QGridLayout(self)
+        vlayout=QVBoxLayout(self)
+
+        #为局部添加控件
+        layout1.addWidget(self.label,0,1,1,2)
+        layout1.addWidget(self.btnAPOD,1,1,1,1)
+        layout1.addWidget(self.btnNEO,1,2,1,1)
+        layout1.addWidget(self.btnMARS,1,3,1,1)
+        layout1.addWidget(self.btnQUIT, 1,4,1,1)
+
+        vlayout.addWidget(self.label2)
+        vlayout.addWidget(self.text)
+
+        #准备两个控件
+        gwg=QWidget()
+        vwg=QWidget()
+
+        #用两个控件设置局部布局
+        vwg.setLayout(vlayout)
+        gwg.setLayout(layout1)
+
+        #将两个控件添加到全局布局中去
+        wlayout.addWidget(vwg)
+        wlayout.addWidget(gwg)
+
+        #将窗口本身设置为全局布局
+        self.setLayout(wlayout)
+
+        # 信号与槽连接, PyQt5与Qt5相同, 信号可绑定普通成员函数
+        self.btnAPOD.clicked.connect(self.APOD)
+        self.btnNEO.clicked.connect(self.Neo)
+        self.btnMARS.clicked.connect(self.Mars)
+        self.btnQUIT.clicked.connect(self.quit)
+
+    #button功能函数
+    def APOD(self):
+        path,imstr,date,explanation=btnfunc.Get_APOD()
+        if path:
+            self.label2.setPixmap(QPixmap(path).scaled(480,680,aspectRatioMode=1))
+            if date and explanation:
+                self.text.setText("date:%s   explanation:%s"%(date,explanation))
+
+        else:
+            self.label2.setText('非图片格式，请访问： %s' %imstr)
+
+
+
+    def Neo(self):
+        i,ok = QInputDialog.getInt(self,'查找小行星','id：',0,0,9999999,1)
+        if ok:
+            id_num=str(i)
+            (name,magnitude,dmin,dmax,L)=btnfunc.Get_NeoInfo(id_num)
+            self.text.clear()  # 清理text
+            if name:
+                self.label2.setText("名称：%s \n绝对星等： %f\n估计直径/km：最小：%f  最大：%f" %(name,magnitude,dmin,dmax))
+                for d in L:
+                    self.text.append("接近日期: %s  轨道天体: %s"%(d['date'],d["orbiting_body"]) )
+            else:
+                self.label2.setText("此id不存在")
+    def Mars(self):
+        rover = ('curiosity','opportunity','spirit')
+        item,ok=QInputDialog.getItem(self,"Mars Rover","请选择探测器：",rover,0,False)
+        if ok:
+            btnfunc.Grt_Mars_Pic(item)
+
+    def quit(self):
+        sys.exit()
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = Window()
+    w.show()
+    sys.exit(app.exec_())
---
+++ btnfunc.py
@@ -0,0 +1,122 @@
+import  sys
+from PyQt5 import QtGui
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication,QPushButton,QDialog,QGridLayout,QLabel,QWidget,QInputDialog,QLineEdit,QMessageBox
+from PyQt5.QtCore import QCoreApplication
+import requests
+import json
+import cv2
+import btnfunc
+
+def Get_key():
+    f=open('key.txt','r')
+    apikey=f.read()
+    f.close()
+    return apikey
+
+def Get_APOD():
+    apikey=Get_key()
+    url = "https://api.nasa.gov/planetary/apod?api_key="+apikey
+    resp = requests.get(url)
+    result = resp.json()
+    mediatype = result["media_type"]
+    # 资源类型为图片
+    if mediatype == "image":
+
+        imstr = result['url']
+
+        img = requests.get(imstr).content
+        path = 'APOD.jpg'
+        with open(path, 'wb')as f:
+            f.write(img)
+        date=result['date']
+        explanation=result['explanation']
+        return path,imstr,date,explanation
+    else:
+        return None,None,None,None
+
+
+
+def Get_NeoInfo(id_num):
+    apikey=Get_key()
+    url = 'https://api.nasa.gov/neo/rest/v1/neo/' + id_num + '?api_key='+apikey
+    resp = requests.get(url)
+    if resp.status_code == 200:
+        result = resp.json()
+        L=[]
+        # 选取指定元素
+        for item in result["close_approach_data"]:
+            d={}
+            d['date']=item["close_approach_date"]
+            d["orbiting_body"]=item["orbiting_body"]
+            L.append(d)
+
+        return (result['name'],result["absolute_magnitude_h"],result["estimated_diameter"]["kilometers"]["estimated_diameter_min"],result["estimated_diameter"]["kilometers"]["estimated_diameter_max"],L)
+    else:
+        return False, False, False, False,False
+
+
+def Grt_Mars_Pic(probe):
+    apikey=Get_key()
+    url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/' + probe + '/photos?sol=1000&page=1&api_key=v'+apikey
+    resp = requests.get(url)
+    if resp.status_code == 200:
+        result = resp.json()
+        pic_id = 0
+        for i in range(6):
+            pic_url = result['photos'][i]['img_src']
+            responce = requests.get(str(pic_url))
+            img = responce.content
+            path = str(pic_id) + '.jpg'
+            with open(path, 'wb')as f:
+                f.write(img)
+                pic_id += 1
+                i += 1
+        newWindow = SecondWindow()
+        newWindow.show()
+        newWindow.exec_()
+
+# 子窗口
+class SecondWindow(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.newWindowUI()
+    def newWindowUI(self):
+
+        self.resize(500,700)
+        self.move(200,200)
+        self.buttonNext=QPushButton('Next',self)
+        self.buttonPre=QPushButton('Pre',self)
+        self.label_pic=QLabel()
+        self.num=0
+        # 布局设定
+        layout2 = QGridLayout(self)
+        layout2.addWidget(self.label_pic, 0, 1, 2, 2)
+        layout2.addWidget(self.buttonNext, 2, 1, 1, 1)
+        layout2.addWidget(self.buttonPre, 2, 2, 1, 1)
+
+        self.label_pic.setPixmap(QPixmap(str(self.num)+'.jpg').scaled(500,500,aspectRatioMode=1))
+
+        self.buttonPre.clicked.connect(self.Previous)
+        self.buttonNext.clicked.connect(self.Next)
+
+    #button功能函数
+    def Previous(self):
+        if self.num==0:
+            # 消息框
+            reply=QMessageBox.information(self,"提示",'没有上一张图片',QMessageBox.Yes,QMessageBox.No)
+            print(reply)
+        else:
+            self.num=self.num-1
+            self.label_pic.setPixmap(QPixmap(str(self.num) + '.jpg').scaled(500,500,aspectRatioMode=1))
+
+    def Next(self):
+        if self.num==5:
+            reply = QMessageBox.information(self, "提示", '没有下一张图片', QMessageBox.Yes, QMessageBox.No)
+            print(reply)
+        else:
+            self.num=self.num+1
+            self.label_pic.setPixmap(QPixmap(str(self.num) + '.jpg').scaled(500,500,aspectRatioMode=1))
+
+
+
